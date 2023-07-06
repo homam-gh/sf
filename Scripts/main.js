550aa249e7376f04109e10c1c2ef1e00772be6e4
Mahdi Ghasemi <mahdighasemi@EUIL-MAC084.local>
2023-07-06 14:11:48 +0200

    initial commit

---
+++ Scripts/main.js
@@ -0,0 +1,59 @@
+const messageContainer = document.getElementById("messageContainer");
+if (messageContainer) {
+  messageContainer.scrollTop = messageContainer.scrollHeight;
+}
+
+window.closeHeader = (id) => {
+  const elm = document.getElementById(id);
+  elm.style.display = "none";
+};
+
+window.toggleMobileHeader = () => {
+  const body = document.getElementsByTagName("body")[0];
+  body.classList.toggle("menu-opened");
+};
+window.toggleThis = (currentElm, id, className) => {
+  const activeState = className || "is-opened";
+  const elm = document.getElementById(id);
+  currentElm.classList.toggle(activeState);
+  elm.classList.toggle(activeState);
+
+  const clickHandler = function (event) {
+    if (!currentElm.contains(event.target) && !elm.contains(event.target)) {
+      currentElm.classList.remove(activeState);
+      elm.classList.remove(activeState);
+
+      document.removeEventListener("click", clickHandler);
+    }
+  };
+
+  document.addEventListener("click", clickHandler);
+};
+
+window.toggleSearch = () => {
+  const elm = document.getElementById("searchForm");
+  elm.classList.toggle("is-opened");
+};
+
+const wheels = document.querySelectorAll(".wheel");
+
+const count = (to, elm) => {
+  elm.classList.add("rotate");
+  const innerElm = elm.getElementsByClassName("wheel-number")[0];
+  let count = 0;
+
+  const int = setInterval(() => {
+    if (count > to) {
+      clearInterval(int);
+      elm.classList.remove("rotate");
+    } else {
+      innerElm.innerHTML = count;
+      count++;
+    }
+  }, 10);
+};
+
+wheels.forEach((wheel) => {
+  const targetNumber = parseInt(wheel.textContent);
+  count(targetNumber, wheel);
+});
